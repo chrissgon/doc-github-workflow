36139ef84c9c0e5e8f648b2c9542ab1d41808208
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-15 15:39:33 -0300

    feat: :sparkles: force fail test

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	expect := 2
+	expect := 3
 	have := sum(1, 1)
 	if expect != have {
 		t.Errorf("expect %d, but have %d", expect, have)
