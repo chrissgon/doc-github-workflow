c14c3f3ad1c97dded232163c2107fa15de2d695f
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-15 15:03:13 -0300

    feat: :sparkles: add methods and test of sum

---
+++ main_test.go
@@ -0,0 +1,12 @@
+// main_test.go
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	expect := 2
+	have := sum(1, 1)
+	if expect != have {
+		t.Errorf("expect %d, but have %d", expect, have)
+	}
+}
