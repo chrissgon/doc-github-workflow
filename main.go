29bfd631cba231ef5988efb428e087de4eed4802
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-16 09:38:01 -0300

    feat: :sparkles: remove prints and adjust tests

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 // main.go
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Printf("2 + 2 = %v \n", sum(2,2))
-	fmt.Printf("2 - 2 = %v \n", sub(2,2))
-	fmt.Printf("2 * 2 = %v \n", mult(2,2))
-	fmt.Printf("2 / 2 = %v \n", div(2,2))
-}
+func main() {}
 
 func sum(a int, b int) int {
 	return a + b
