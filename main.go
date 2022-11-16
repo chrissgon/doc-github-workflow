7ce9e753f7f2a577975ff61a2f182809543d2cef
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-16 08:21:52 -0300

    feat: :sparkles: add tests and methods

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 // main.go
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	fmt.Printf("2 + 2 = %v \n", sum(2,2))
+	fmt.Printf("2 - 2 = %v \n", sub(2,2))
+	fmt.Printf("2 * 2 = %v \n", mult(2,2))
+	fmt.Printf("2 / 2 = %v \n", div(2,2))
+}
 
 func sum(a int, b int) int {
 	return a + b
--- main_test.go
+++ main_test.go
@@ -4,29 +4,29 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	expect := 2 // the correct one is 2
-	have := sum(1, 1)
+	expect := 4 // the correct one is 2
+	have := sum(2, 2)
 	if expect != have {
 		t.Errorf("expect %d, but have %d", expect, have)
 	}
 }
 func TestSub(t *testing.T) {
-	expect := 2
-	have := sub(3, 1)
+	expect := 0
+	have := sub(2, 2)
 	if expect != have {
 		t.Errorf("expect %d, but have %d", expect, have)
 	}
 }
 func TestMult(t *testing.T) {
-	expect := 2
-	have := mult(2, 1)
+	expect := 4
+	have := mult(2, 2)
 	if expect != have {
 		t.Errorf("expect %d, but have %d", expect, have)
 	}
 }
 func TestDiv(t *testing.T) {
-	expect := 2
-	have := div(4, 2)
+	expect := 1
+	have := div(2, 2)
 	if expect != have {
 		t.Errorf("expect %d, but have %d", expect, have)
 	}
