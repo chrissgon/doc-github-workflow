2c9f19b0e41bb60ecc370176839ed01d1c7b9eca
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-16 08:02:52 -0300

    test: :white_check_mark: add tests

--- main_test.go
+++ main_test.go
@@ -4,9 +4,30 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
- expect := 2 // the correct one is 2
- have := sum(1, 1)
- if expect != have {
-  t.Errorf("expect %d, but have %d", expect, have)
- }
+	expect := 2 // the correct one is 2
+	have := sum(1, 1)
+	if expect != have {
+		t.Errorf("expect %d, but have %d", expect, have)
+	}
+}
+func TestSub(t *testing.T) {
+	expect := 2
+	have := sub(3, 1)
+	if expect != have {
+		t.Errorf("expect %d, but have %d", expect, have)
+	}
+}
+func TestMult(t *testing.T) {
+	expect := 2
+	have := mult(2, 1)
+	if expect != have {
+		t.Errorf("expect %d, but have %d", expect, have)
+	}
+}
+func TestDiv(t *testing.T) {
+	expect := 2
+	have := div(4, 2)
+	if expect != have {
+		t.Errorf("expect %d, but have %d", expect, have)
+	}
 }
